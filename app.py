2829d66818e148365772fe6fbb07a4b8855208d4
yuichi18 <yuichi.heguri18@gmail.com>
2023-02-21 03:58:22 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+# タイトル  
+st.title('タイトル')
+# ヘッダー  
+st.header('ヘッダー')
+# サブヘッダー  
+st.subheader('サブヘッダー')
+# テキスト  
+st.text('テキスト')
+# マークダウン  
+st.markdown('これは **マークダウン**です。')
